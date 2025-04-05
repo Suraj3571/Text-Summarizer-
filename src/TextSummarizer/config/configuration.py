70b148e1a42e368c2e75db3f6c8adbd9a8a6dc67
Suraj <swaragade2@gmail.com>
2025-04-05 15:38:33 +0530

    Data Transformation done

--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -2,7 +2,7 @@
 
 from src.TextSummarizer.constants import *
 from src.TextSummarizer.utils.common import read_yaml, create_directories
-from src.TextSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
+from src.TextSummarizer.entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig)
 
 ## Data ingestion
 class ConfigurationManager:
@@ -42,6 +42,19 @@ class ConfigurationManager:
         ) 
         
         return data_validation_config   
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+        
+        create_directories([config.root_dir])
+        
+        data_transformation_config = DataTransformationConfig(
+            root_dir = config.root_dir, 
+            data_path = config.data_path, 
+            tokenizer_name = config.tokenizer_name
+        )    
+        
+        return data_transformation_config
 
     
         
