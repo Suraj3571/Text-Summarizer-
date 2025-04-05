47a4c9feb7e9b3fa4b5e7e7c7941c51ecef6e0ed
Suraj <swaragade2@gmail.com>
2025-04-05 13:37:58 +0530

    Data validation component added

--- src/TextSummarizer/config/configuration.py
+++ src/TextSummarizer/config/configuration.py
@@ -2,9 +2,9 @@
 
 from src.TextSummarizer.constants import *
 from src.TextSummarizer.utils.common import read_yaml, create_directories
-from src.TextSummarizer.entity import (DataIngestionConfig)
+from src.TextSummarizer.entity import (DataIngestionConfig, DataValidationConfig)
 
-
+## Data ingestion
 class ConfigurationManager:
     def __init__(
         self, 
@@ -29,5 +29,20 @@ class ConfigurationManager:
         ) 
         
         return data_ingestion_config   
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        
+        create_directories([config.root_dir])
+        
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir, 
+            STATUS_FILE = config.STATUS_FILE, 
+            ALL_REQUIRED_FILES = config.ALL_REQUIRED_FILES
+        ) 
+        
+        return data_validation_config   
+
+    
         
         
